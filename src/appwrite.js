91f4a04317b6f28addf864c94ec412cba05a1961
Adenike <adeyinkaadenike992@gmail.com>
2024-07-18 16:04:20 +0100

    commited

---
+++ src/appwrite.js
@@ -0,0 +1,24 @@
+// import { Client } from 'appwrite';
+
+// const client = new Client();
+
+// client
+//     .setEndpoint('https://cloud.appwrite.io/v1')
+//     .setProject('66992be600306ff111e1');
+
+// const account = new Account(client);
+
+// export { account };
+
+
+import { Client, Account } from 'appwrite';
+
+const client = new Client();
+
+client
+       .setEndpoint('https://cloud.appwrite.io/v1')
+       .setProject('66992be600306ff111e1');
+
+const account = new Account(client);
+
+export { account };
