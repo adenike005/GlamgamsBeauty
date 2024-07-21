a779f1dbcc31c8cea3ad494f9e38888ff6b8b009
Adenike <adeyinkaadenike992@gmail.com>
2024-07-21 16:10:00 +0100

    commited

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import '../Style/Navbar.css'
+
+import { IoPerson, IoPersonCircle, IoPersonAdd, IoPersonOutline,  IoCart } from 'react-icons/io5';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+
 function Navbar() {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click)
   return (
-   <div className="navbar_Container">
-    <div className="navbar_Logo">
-        <span>GlamgamsBeauty</span>
+  <div className="header">
+    <Link to='/'>
+    <h4>GlamgamsBeauty</h4>
+    </Link>
+
+    <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li><Link>Home</Link></li>
+        <li><Link>About</Link></li>
+        <li><Link>Dashboard</Link></li>
+        <li><Link>Products</Link></li>
+    </ul>
+
+    <div className="header_icon">
+  <IoPersonCircle size={30} color="#fff" className='Icon' />
+  <IoCart size={30} color="#fff" title="Cart"  className='Icon'/>
+
+  <div className="hamburger" onClick={handleClick}>
+    {click ? <FaTimes size={24} color="#fff"/> : <FaBars size={24} color="#fff"/>}
+       
     </div>
-    <div className="navbar_Menu">
-        <ul>
-            <li><Link>Home</Link></li>
-            <li><Link>About</Link></li>
-            <li><Link>Products</Link></li>
-            <li><Link>Dashboard</Link></li>
-        </ul>
     </div>
-    <div className="navbar_Icon">
-        
-    </div>
-   </div>
+
+   
+
+  </div>
   )
 }
 
 export default Navbar
+
---
+++ src/Data/Datas.jsx
@@ -0,0 +1,22 @@
+export default[
+    {
+        "id": 1,
+        "name": "Item 1",
+        "image": "https://via.placeholder.com/150?text=Item+1"
+      },
+      {
+        "id": 2,
+        "name": "Item 2",
+        "image": "https://via.placeholder.com/150?text=Item+2"
+      },
+      {
+        "id": 3,
+        "name": "Item 3",
+        "image": "https://via.placeholder.com/150?text=Item+3"
+      },
+      {
+        "id": 4,
+        "name": "Item 4",
+        "image": "https://via.placeholder.com/150?text=Item+4"
+      }
+]
