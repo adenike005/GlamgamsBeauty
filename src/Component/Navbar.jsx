6d0bc38150a150810593daec97a90faaedf6acca
Adenike <adeyinkaadenike992@gmail.com>
2024-07-22 12:58:17 +0100

    commited

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,43 +1,91 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import '../Style/Navbar.css'
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import "../Style/Navbar.css";
+// import { List, X , ShoppingCart, User, MagnifyingGlass} from '@phosphor-icons/react';
 
-import { IoPerson, IoPersonCircle, IoPersonAdd, IoPersonOutline,  IoCart } from 'react-icons/io5';
-import { FaBars, FaTimes } from 'react-icons/fa';
+// function Navbar() {
+//   return (
+//     <div className="header">
+//       <ul className="nav-menu">
+//         <li><Link>Shop </Link></li>
+//         <li><Link>DashBoard </Link></li>
+//         <li><Link>Shop </Link></li>
+//       </ul>
+//       <div className="logo">GemGlam</div>
+//       <div className="icon">
+//        <div className="box">
+//        {/* <input  type="name"/> */}
+//        <span>Search Product's Here</span>
+//        <MagnifyingGlass size={20} color="#000"/>
+//        </div>
+//       <div className="icons">
+//       <ShoppingCart size={20} color="#000"/>
+//       <User size={20} color="#000"/>
+//       </div>
+//       </div>
+//     </div>
+//   );
+// }
 
+// export default Navbar;
+
+
+
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Style/Navbar.css";
+import { List, X ,ShoppingCart, User, MagnifyingGlass } from '@phosphor-icons/react';
 
 function Navbar() {
 
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click)
+  const [click , setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  // State for managing the search query
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Handler for search input change
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Handler for form submission
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Perform the search action, e.g., navigate to a search results page
+    console.log("Search for:", searchQuery);
+    // For example, you might navigate to a search results page
+    // navigate(`/search?query=${searchQuery}`);
+  };
+
   return (
-  <div className="header">
-    <Link to='/'>
-    <h4>GlamgamsBeauty</h4>
-    </Link>
-
-    <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li><Link>Home</Link></li>
-        <li><Link>About</Link></li>
-        <li><Link>Dashboard</Link></li>
-        <li><Link>Products</Link></li>
-    </ul>
-
-    <div className="header_icon">
-  <IoPersonCircle size={30} color="#fff" className='Icon' />
-  <IoCart size={30} color="#fff" title="Cart"  className='Icon'/>
-
-  <div className="hamburger" onClick={handleClick}>
-    {click ? <FaTimes size={24} color="#fff"/> : <FaBars size={24} color="#fff"/>}
+    <div className="header">
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li><Link to="/shop">Shop</Link></li>
+        <li><Link to="/dashboard">DashBoard</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ?  <X size={16} color="#434242eb" /> : <List size={16} color="#434242eb" />}
+        
        
-    </div>
-    </div>
+      </div>
+      <div className="logo">GemGlam</div>
+      <div className="icon">
+      
+        <div className="icons">
+          <div className="iconbox">
 
-   
+          <ShoppingCart size={16} color="#434242eb"/>
+          </div>
+          <div className="iconbox">
 
-  </div>
-  )
+          <User size={16} color="#434242eb" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
-
+export default Navbar;
